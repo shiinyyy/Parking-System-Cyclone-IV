3cc6e872f831a079b15e808a639ce37008fd98fb
Minh Duc Do <133333040+shiinyyy@users.noreply.github.com>
2024-11-06 19:27:25 +1100

    Update lcd_top.v

--- lcd_top.v
+++ lcd_top.v
@@ -1,8 +1,7 @@
 module lcd_top (
 	input wire	clk,
 	input wire	rst,
-	
-	inout wire	scl,
+	input wire	scl,
 	inout wire	sda
 );
 
@@ -16,41 +15,32 @@ module lcd_top (
     master_i2c master_i2c_inst(
         .clk				(clk),
         .rst				(rst),
-	.data				(data),
-	.rs				(rs),
-	.rw				(rw),
-	.en				(en)
-/*      .start				(start),
-        .data_in			(lcd_data),
         .sda				(i2c_sda),
         .scl				(i2c_scl) 
-		  
-*/
     );
 	 
-	 lcd_i2c lcd_i2c_inst(
-			.clk			(clk),
-			.rst_n			(rst_n),
-			.lcd_e			(lcd_e),
-			.lcd_rs			(lcd_rs),
-			.lcd_data		(lcd_data)
-	  );
+    lcd_i2c lcd_i2c_inst(
+	.clk				(clk),
+	.lcd_data		(lcd_data),
+	.sda				(i2c_sda),
+	.scl				(i2c_scl)
+    );
 
     // LCD data to display ""
-    reg [7:0] lcd_data_reg;
+    reg [7:0] data_reg;
     reg start_reg;
     
-    always @(posedge clk or negedge rst_n) begin
-        if (!rst_n) begin
-            lcd_data_reg <= 8'h1; 
-            start_reg <= 1'b0;
+    always @(posedge clk or negedge sda) begin
+        if (!sda) begin
+            data_reg <= 8'b0; 
+            start_reg <= 8'b0;
         end else begin
-            lcd_data_reg <= 8'h50;
-            start_reg <= 1'b1;     // Start
+            data_reg <= 8'b1;
+            start_reg <= 8'b1;     // Start
         end
     end
     
-    assign lcd_data = lcd_data_reg;
-    assign start = start_reg;
+    assign lcd_data = data_reg;
+ //   assign start = start_reg;
 
 endmodule
